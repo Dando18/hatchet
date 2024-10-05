deceb26e2e7360174eabbea8fe49789f4b09f230
Daniel Nichols <dando18studios@gmail.com>
2024-10-05 16:38:17 -0700

    format to black formatting

--- hatchet/writers/pyg_writer.py
+++ hatchet/writers/pyg_writer.py
@@ -4,13 +4,12 @@
 # SPDX-License-Identifier: MIT
 
 
-
 class PyGWriter:
-    """ Create PyTorch Geometric Data graph object from a GraphFrame.
+    """Create PyTorch Geometric Data graph object from a GraphFrame.
 
-        Creates a node for each nid in the GraphFrame. Uses an encoding of the
-        node name as the value for each node. Also includes metric values in the
-        encoding.
+    Creates a node for each nid in the GraphFrame. Uses an encoding of the
+    node name as the value for each node. Also includes metric values in the
+    encoding.
     """
 
     def __init__(self):
@@ -19,44 +18,48 @@ class PyGWriter:
     def _build_node_representation(self, node, metric_columns, dataframe, encoder):
         embedding = dataframe.loc[node, metric_columns].values.tolist()
         if encoder:
-            name = dataframe.loc[node, 'name']
+            name = dataframe.loc[node, "name"]
             encoding = encoder(name)
             embedding.extend(encoding)
         return embedding
 
-    def _build_graph(self, node, visited, nodes, edges, metric_columns, dataframe, encoder):
+    def _build_graph(
+        self, node, visited, nodes, edges, metric_columns, dataframe, encoder
+    ):
         node_id = node._hatchet_nid
 
-        embedding = self._build_node_representation(node, metric_columns, dataframe, encoder)
+        embedding = self._build_node_representation(
+            node, metric_columns, dataframe, encoder
+        )
         nodes[node_id] = embedding
 
         visited.append(node)
         for child in node.children:
             child_id = child._hatchet_nid
             if child not in visited:
-                self._build_graph(child, visited, nodes, edges, metric_columns, dataframe, encoder)
+                self._build_graph(
+                    child, visited, nodes, edges, metric_columns, dataframe, encoder
+                )
             edges.append((node_id, child_id))
 
-    def write(
-        self, 
-        gf, 
-        metrics=['time', 'time (inc)'], 
-        encoder=None,
-        **kwargs
-    ):
-        """ Return a PyG Data object from a GraphFrame. """
+    def write(self, gf, metrics=["time", "time (inc)"], encoder=None, **kwargs):
+        """Return a PyG Data object from a GraphFrame."""
         import torch
         from torch_geometric.data import Data
 
         gf = gf.deepcopy()
 
         if any([metric not in gf.dataframe.columns for metric in metrics]):
-            raise ValueError(f"Some metrics in {metrics} are missing from the dataframe.")
+            raise ValueError(
+                f"Some metrics in {metrics} are missing from the dataframe."
+            )
 
         # check if 'rank' or 'thread' are in the index
         if "rank" in gf.dataframe.index.names or "thread" in gf.dataframe.index.names:
             # TODO -- add support for rank and thread
-            raise ValueError("PyGWriter does not support 'rank' or 'thread' in the index.")
+            raise ValueError(
+                "PyGWriter does not support 'rank' or 'thread' in the index."
+            )
 
         # assert that if encoder is not None/False, then it must be callable
         if encoder and not callable(encoder):
@@ -64,12 +67,14 @@ class PyGWriter:
 
         nodes = [None] * len(gf.dataframe)
         edges = []
-        weights = None # TODO -- use call information to weight edges
+        weights = None  # TODO -- use call information to weight edges
 
         visited = []
         for root in gf.graph.roots:
-            self._build_graph(root, visited, nodes, edges, metrics, gf.dataframe, encoder)
-        
+            self._build_graph(
+                root, visited, nodes, edges, metrics, gf.dataframe, encoder
+            )
+
         nodes = torch.tensor(nodes, dtype=torch.float)
         edges = torch.tensor(edges, dtype=torch.long).t().contiguous()
         return Data(x=nodes, edge_index=edges, edge_attr=weights)
